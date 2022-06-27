7e69a04361aaa74310aa326729d34de74324c7fb
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-27 23:47:20 +0500

    Create Navbar & give it styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FaBars, FaCog, FaMicrophone } from 'react-icons/fa'
+import './Navbar.css'
+
+export default class Navbar extends React.Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render = () => (
+    <>
+      <header>
+        <FaBars className="nav-icon">
+          <h1>Covid Tracker</h1>
+          <div className="navBar--rightSide">
+            <FaMicrophone className="nav-icon" />
+            <FaCog className="nav-icon" />
+          </div>
+        </FaBars>
+      </header>
+    </>
+  )
+}
